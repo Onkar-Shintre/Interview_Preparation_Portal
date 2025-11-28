db35db1eda76c2422a19df01dc7c9d09c475a0b6
Onkar Shintre <shintreomkar07@gmail.com>
2025-11-28 10:47:16 +0530

    Update db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -6,7 +6,7 @@ const connectDB = async () => {
         console.log("MongoDB connected");
     } catch (err) {
         console.error("Error connecting to MongoDB", err);
-        process.exit(1);
+        process.exit(1);   //db
     }
 };
 
